b148a4b3a0552df8f1c9810b00454bc319582f73
Vishal Garad <garad_v@India.XoriantCorp.com>
2021-09-30 12:34:09 +0530

    first commit

---
+++ src/main/java/com/jenkins/SpringJenkinsPackagingJarApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsPackagingJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsPackagingJarApplication.class, args);
+	}
+
+}
